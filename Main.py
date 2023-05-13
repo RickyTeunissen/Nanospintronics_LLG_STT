6bf38e53ceaaa96e4e2c4f371e8f6392edb47888
Ricky <r.m.p.teunissen@student.tue.nl>
2023-05-13 14:46:23 +0200

    playing a bit with branches

--- Main.py
+++ Main.py
@@ -1,11 +1,19 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import scipy
-# just quickly test if important stuff is working
+import time
 
+#just some test stuff, can later all be removed
 print("Hello world")
 print("Programmed to work and not to feeeeel!")
 
 test = np.linspace(1, 20)
 print(test)
+
+
+fig,ax = plt.subplots(2,2, figsize = (10,5))
+ax[0,0].plot(test, test**2)
+ax[0,0].set_xlabel("lol")
+ax[0,0].set_ylabel("hihi")
+plt.show()
 
