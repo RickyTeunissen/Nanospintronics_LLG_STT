54fb44b996cd19143c7066b1a366ba22e29482d0
Ricky <r.m.p.teunissen@student.tue.nl>
2023-05-13 14:32:26 +0200

    Played around some more and added a requirements.txt file to automatically ensure we have the same libraries installed in our own venv

--- Main.py
+++ Main.py
@@ -1,1 +1,11 @@
-print("Just testing for now ")
+import numpy as np
+import matplotlib as plt
+import scipy
+# just quickly test if important stuff is working
+
+print("Hello world")
+print("Programmed to work and not to feeeeel!")
+
+test = np.linspace(1, 20)
+print(test)
+
