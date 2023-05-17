Solved some bugs due to wrong units
and started testing with spin current stuff numbers and their effect

LLG_solver.py
@@ -13,10 +13,13 @@ from matplotlib.ticker import FormatStrFormatter
 from matplotlib.ticker import MultipleLocator
 
 mu0: float = constants.mu_0  # [N/A^2]
-gyro_ratio: float = 1.760896 * 10 ** 10  # [A/m]
 hbar: float = constants.hbar
-me: float = constants.electron_mass
-#gyro_ratio: float = constants.elementary_charge/(2*me)  # [A/m]
+me: float = constants.electron_mass  # [kg]
+charge: float = constants.elementary_charge  # [C]
+gyro_ratio: float = 2 * charge / (2 * me)  # [A/m]
+
+
+# gyro_ratio: float = 1.760896 * 10 ** 11  # [A/m]
 
 
 def calc_effective_field(m3d: tuple, Hext: np.array, Ms: float) -> tuple:
@@ -47,7 +50,7 @@ def calc_total_spin_torque():
 
 def N(J: float, d: float, m3d: np.array, M3d: np.array) -> np.array:
     eta = 1
-    N_spin_transfer = np.cross(eta*(hbar/(2*me))*(J/d)*m3d, np.cross(m3d, M3d))
+    N_spin_transfer = eta * (hbar / (2 * charge)) * (J / d) * np.cross(m3d, np.cross(m3d, M3d))
     return N_spin_transfer
 
 
@@ -77,7 +80,8 @@ def LLG(t, m3d: tuple, Hext: np.array, alpha: float, Ms: float, J: float, d: flo
     return dmx, dmy, dmz
 
 
-def LLG_solver(IC: tuple, t_points: np.array, Hext: np.array, alpha: float, Ms: float, J: float, d: float, M3d: tuple) -> tuple:
+def LLG_solver(IC: tuple, t_points: np.array, Hext: np.array, alpha: float, Ms: float, J: float, d: float,
+               M3d: tuple) -> tuple:
     """
     Solves the LLG equation for given IC and parameters in the time range t_points
 
@@ -104,6 +108,12 @@ def LLG_solver(IC: tuple, t_points: np.array, Hext: np.array, alpha: float, Ms: 
 
 
 def polarToCartesian(r: float, theta: float, phi: float) -> np.array:
+    """
+    :param r: distance to origin [m]
+    :param theta: angle with z axis in [radians]
+    :param phi: angle with x axis in [radians]
+    :return:
+    """
     x = r * np.sin(theta) * np.cos(phi)
     y = r * np.sin(theta) * np.sin(phi)
     z = r * np.cos(theta)
@@ -114,11 +124,11 @@ def plotResult(mx: np.array, my: np.array, mz: np.array, m0: np.array, t: np.arr
     f = plt.figure(1)
     axf = f.add_subplot(projection="3d")
     axf.plot(mx, my, mz, "b-")
-    axf.scatter(m0[0], m0[1], m0[2], color="red", lw=10)
+    axf.scatter(m0[0], m0[1], m0[2], color="red", lw=5)
     axf.set_xlabel("mx")
     axf.set_ylabel("my")
     axf.set_zlabel("mz")
-    axf.set_title("Some stupid example")
+    axf.set_title("Magnetization direction over time")
     axf.xaxis.set_major_locator(MultipleLocator(0.5))
     axf.yaxis.set_major_locator(MultipleLocator(0.5))
     axf.zaxis.set_major_locator(MultipleLocator(0.5))
@@ -146,15 +156,14 @@ if __name__ == "__main__":
     # let's run an example of the LLG solver for some IC.
     # NOTE: MUST ALWAYS START AT SOME ANGLE AS ELSE MAYBE ISSUE's when temperature = 0
 
-    Hext = np.array([0.2e6, 0, 0])  # [A/m]
-    m0 = polarToCartesian(1, 0.4*np.pi, 0)
+    Hext = np.array([-3.9e4, 0, 0])  # [A/m]
+    m0 = polarToCartesian(1, 0.49*np.pi, 0.1)
     alpha = 0.1
     Ms = 1.27e6  # [A/m]
-    J = 0.0001  # [???] some form of A/m^2
-    d = 1e-9  # [m]
-    M3d = polarToCartesian(1, 0, 0)
-    t = np.arange(0, 2.5e-9, 1e-12)
+    J = 1e8  # [A/m^2]
+    d = 3e-9  # [m]
+    M3d = np.array([0, 0, 1]) #polarToCartesian(1, 0.5*np.pi, 0)
+    t = np.arange(0, 0.5e-10, 5e-16)
     mx, my, mz = LLG_solver(m0, t, Hext, alpha, Ms, J, d, M3d)
 
     plotResult(mx, my, mz, m0, t)
-