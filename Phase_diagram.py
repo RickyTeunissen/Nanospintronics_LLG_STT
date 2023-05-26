3850a05530f9268d8ef2840ff1f043019a8714d2
RFJ-Rik <72444586+RFJ-Rik@users.noreply.github.com>
2023-05-26 23:24:16 +0200

    Create phase diagram generator with multiprocessing.

--- Phase_diagram.py
+++ Phase_diagram.py
@@ -1,36 +1,27 @@
 """
-Within this file, all the code necisarry for constructing phase diagrams will be implemented
+Within this file, all the code necesarry for constructing phase diagrams will be implemented
 """
 import numpy as np
 import LLG_solver as LLG
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from multiprocessing import Pool
+from functools import partial
+import os
 
 
-def SingleLine():
-    # defining relevant system parameters:
-    Hext = np.array([-5e5, 0, 0])  # [A/m]
-    m0 = np.array([1, 0, 0])  # polarToCartesian(1, 0.49 * np.pi, 0.1)
-    alpha = 0.1
-    Ms = 1.27e6  # [A/m]
-    d = 3e-9  # [m]
-    area = 130e-9 * 70e-9
-    M3d = np.array([0, 0, 1])  # polarToCartesian(1, 0.5*np.pi, 0)
-    temperature = 300  # [K], note: need like 1e5 to see really in plot (just like MATLAB result)
-
-    # which t points solve for, KEEP AS ARANGE (need same distance between points)!!
-    t = np.arange(0, 5e-10, 1e-12)
-    Jarray = np.linspace(-0.5e6, 0.6e6, 10)
+def SingleLine(m0: np.array, t_points: np.array, Hext: float, alpha: float, Ms: float, thickness: float,
+               width_x: float, width_y:float, temp: float, M3d: np.array, K_surf: float, useMemory: bool, skipLength: int):
     resultDictionary = {}
-    skipLength = int(np.floor(len(t) * (2 / 3)))
-    inspectionT = t[skipLength:]
-    totLength = len(Jarray)
+
     for index, J in enumerate(Jarray):
         # solving the system
-        mx, my, mz = LLG.LLG_solver(m0, t, Hext, alpha, Ms, J, d, area, temperature, M3d)
+        if useMemory & index > 0:
+            m0 = np.array([mx[-1], my[-1], mz[-1]])
+        HextFinal = np.array([Hext, 0, 0])
+        mx, my, mz = LLG.LLG_solver(m0, t_points, HextFinal, alpha, Ms, J, thickness, width_x, width_y, temp, M3d, K_surf)
         inspectMx, inspectMy, inspectMz = mx[skipLength:], my[skipLength:], mz[skipLength:]
         resultDictionary.update({J: [inspectMx, inspectMy, inspectMz]})
-        print(f"\r Working on step {index+1}/{totLength}", end="")
-
-    print("\n finished")
     return resultDictionary
 
 
@@ -38,10 +29,100 @@ def LineAnalysis(inputDictionary):
     resultDictionary = {}
     for J, values in inputDictionary.items():
         mz = values[2]
-        resultDictionary.update({J: np.average(mz)})
+        x = np.average(mz).round(2)
+        choiceList = [1, 2, 3, 2, 5]  # P, OPP, IPP, OPP, AP
+        conditionList = [x >= 0.8, 0.2 <= x < 0.8, -0.2 < x < 0.2, -0.8 < x <= -0.2, x <= -0.8]
+        categorizedValues = np.select(conditionList, choiceList)
+        resultDictionary.update({J: x})
     return resultDictionary
 
 
-result = SingleLine()
-finalResult = LineAnalysis(result)
-print(finalResult)
+def SweepH(m0: np.array, t_points: np.array, alpha: float, Ms: float, thickness: float, width_x: float,
+        width_y: float, temp: float, M3d: np.array, K_surf: float, useMemory: bool, skipLength: int, totLength: float,
+           HextX: float, HextY: float, HextZ: float):
+    global progress
+    result = SingleLine(
+        m0, t_points, HextX, alpha, Ms, thickness, width_x, width_y, temp, M3d, K_surf, useMemory, skipLength)
+    analyzedResult = LineAnalysis(result)
+    # phaseDiagramDictionary.update({Hext[0]: analyzedResult})
+    progress += 1
+    print(f"\r Working on step {progress}/{totLength} of the field", end="")
+    return analyzedResult
+
+def TotalDiagram(
+        m0: np.array, t_points: np.array, HextArray: np.array, alpha: float, Ms: float, thickness: float,
+        width_x: float, width_y: float, temp: float, M3d: np.array, K_surf: float, useMemory: bool, skipLength: int, pool):
+    totLength = len(HextArray[0])
+    partialSweepH = partial(SweepH, m0, t_points, alpha, Ms, thickness, width_x,
+                            width_y, temp, M3d, K_surf, useMemory, skipLength, totLength)
+    results = pool.starmap(partialSweepH, [x for x in HextArray][0].tolist())
+
+    print("\n finished with the field")
+    return results
+
+
+def packagingForPlotting(inputList, gridSize: int):
+    stateArray = np.zeros((gridSize, gridSize))
+    for index, currentDictionary in enumerate(inputList):
+        currentArray = []
+        for current, state in currentDictionary.items():
+            currentArray.append(state.tolist())
+        stateArray[index] = currentArray
+    return stateArray
+
+
+global progress
+progress = 0
+# defining relevant system parameters:
+alpha = 0.01  # SHOULD BE 0.01 FOR Cu!
+Ms = 1.27e6  # [A/m]
+K_surface = 0.5e-3  # J/m^2
+d = 3e-9  # [m]
+width_x = 130e-9  # [m] need width_x > width_y >> thickness (we assume super flat ellipsoide)
+width_y = 70e-9  # [m]
+temperature = 3  # [K], note: need like 1e5 to see really in plot (just like MATLAB result)
+
+# initial direction free layer and fixed layer
+m0 = np.array([1, 0, 0])
+M3d = np.array([1, 0, 0])
+
+# which t points solve for, KEEP AS ARANGE (need same distance between points)!!
+t = np.arange(0, 5e-9, 5e-12)
+gridSize = 50
+Jarray = np.linspace(-0.5e12, 0.5e12, gridSize)
+HextX = np.linspace(-5.5e4, 5.5e4, gridSize)  # [A/m]
+HextY = np.linspace(0, 0, gridSize)  # [A/m]
+HextZ = np.linspace(0, 0, gridSize)  # [A/m]
+HextArray = np.dstack([HextX, HextY, HextZ])
+
+skipLength1 = int(np.floor(len(t) * (3 / 4)))
+inspectionT = t[skipLength1:]
+
+
+if __name__ == '__main__':
+    try:
+        pool = Pool(os.cpu_count())
+        phaseDictionary = TotalDiagram(np.array([1, 0, 0]), t, HextArray, alpha, Ms, d, width_x,
+                     width_y, temperature, M3d, K_surface, False, skipLength1, pool)
+    finally:
+        pool.close()
+        result = packagingForPlotting(phaseDictionary, gridSize)
+        X, Y = np.meshgrid(Jarray, HextX)  # Yes we also need to turn into meshgrid
+
+        fig2 = plt.figure(figsize=(6, 6))
+        plt.pcolormesh(X, Y, result, cmap=cm.Blues, )
+        plt.colorbar()
+
+        plt.show()
+
+#phaseDictionary = TotalDiagram(
+#    np.array([1, 0, 0]), t, HextArray, alpha, Ms, d, width_x, width_y, temperature, M3d, K_surface, False, skipLength1)
+#result = packagingForPlotting(phaseDictionary, gridSize)
+
+#X, Y = np.meshgrid(Jarray, HextX)  # Yes we also need to turn into meshgrid
+
+#fig2 = plt.figure(figsize=(6, 6))
+#plt.pcolormesh(X, Y, result, cmap=cm.Blues, )
+#plt.colorbar()
+
+#plt.show()
