30cfaaba46ffd9f07d5b87b9687150650c605fe6
Ricky <r.m.p.teunissen@student.tue.nl>
2023-06-06 16:39:07 +0200

    Updated the read me file and Played around a bit with the temperature histogram stuff to test stuff

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Nanospintronics_LLG_STT
 Created as part of the course Nanospintronics (3MN220) at the technical University of Eindhoven (TUE)
 - Authors: R.M.P. Teunissen, R.F.J. van Haren
-- Date last updated: May 2023
+- Date last updated: June 2023
 
 ## About this project
-The code of this file has been written as part of the course Nanospintronics at the TUE 
+The code of this file has been written as part of the course Nanospintronics at the TUE (Einhoven, Netherlands).
+The code in this project can be used to simulate the dynamics of MRAM bits according to the LLG equation, create phase 
+diagrams showing the dynamic type for various field and current, add a custom current shape.
 
 The system at hand consists of a thin nanopillar consisting of a thick "pinned" ferromagnetic layer 
 followed by a thin conducting layer and a thin "free" ferromagnetic layer.
@@ -12,17 +14,29 @@ The exact stack composition is:
 
 Cu(80nm)/Co(40nm)/Cu(thin)/Co(3nm)/Cu(10nm)
 
-The coordinate system is define by:
+The coordinate system is define by (positive direction of Hext is opposite):
 
 ![](Images/new_coord_sys.png)
 
-## Files and necessities
-This project consists of multiple files each with their purpose:
-- main.py .... 
-- .....py can be run to ....
-- .....py constains a series of relevant functions
-- .
+Most of the phenomological constants used in this repository are copied from: DOI: [10.1103/PhysRevB.72.014446]
 
-## important considerations:
-- Maybe renormalize H_temp 
-- is alpha 0.01?
+## About the model:
+The model we use solves the dynamics of the system under the effective of and effective field and a sine like-slowensci spin transfer torque.
+We take a macrospin approach where the entire magnetic layer is assumed to be so small that the magnetization of the layer will rotate as a whole.
+Things which are included are:
+- Precession and damping around/towards an effective field consisting of:
+  - An external magnetic field
+  - A demagnetization field for a thin elliptical cylinder (=shape anisotropy)
+  - A surface anisotropy 
+  - A stochastic temperature field
+- Symmetric slowensci spin transfer torque 
+
+## About the Files and using this code
+This project consists of multiple files each with their purpose, the most important ones are:
+- LLG_solver: contains all important functions for solving the LLG equation, can also be run on its own to investigate the dynamics at given input parameters
+- Phase_diagram.py: Can be run to create a phase diagram of the system. It does so by simulaating the LLG equation for various strenghts of the input current and field and after a while looking at the state the system is in. Alyternatively one can also set ... to TRUE to get a phase diagram showing the maximum angle achieved during the precession to e.g. investigate the effect of AC currents
+
+
+other files we used for testing:
+- speed_test.py: can be run to analyse the time needed for any function and its subfunctions to run, nice for speed analysis
+- Temp_histogram: can be run to check whether the stochastic field will eventually produce a boltzman distribution, note that in order for this to work, we must MANUALLY set the demag_tensor to np.array([0,0,0]) in the LLG_solver 
--- Temp_histogram.py
+++ Temp_histogram.py
@@ -2,7 +2,7 @@
 Using this file we can just quickly run th LLG solver in presence of only a field and some temperature and look
 at the histogram of Mz values encountered to ensure that the right thermal distrubution is obtained using our code
 
-NOTE:
+NOTEEEEEE!:
 To make this program work, we need to turn of all anisotropies, also e.g. shape anisotropy, the easiest way to do this
 is to go to the LLG solver code and manually set the demag tensor to [0,0,0]
 """
@@ -14,65 +14,70 @@ from scipy.optimize import curve_fit
 
 
 def fit_func(x, Temp, A):
+    """
+    the function used to fit (in our case a boltzmann distribution)
+    returns the right hand sife of y = ....
+    """
     kb = scipy.constants.Boltzmann
-    return A*np.exp(-x/(kb*Temp))
+    return A * np.exp(-x / (kb * Temp))
+
 
 if __name__ == "__main__":
-    # let's run an example of the LLG solver for some IC.
-    # NOTE: MUST ALWAYS START AT SOME ANGLE WHEN TEMPERATURE=0
+    # ##########################################################################################
+    # ########## MANUALLY SET DEMAG TENSOR TO [0,0,0] IN LLG SOLVER!!! #########################
+    # ##########################################################################################
+
     print("Starting trajectory calculation....")
     start = time.time()
 
-    # defining relevant system parameters:
+    # ################## defining relevant system parameters:     ##########################
     # need to have no shape anisotropy and no STT and no surface ani and.... basically only some Hext and T
     Hext = np.array([3.9808e4, 0, 0])  # 50mT in [A/m]
-    alpha = 0.1
+    alpha = 0.3 # note that if damping a lot smaller, system relaxes slower s.t. need even longer runtime
     Ms = 1.27e6  # [A/m]
     K_surface = 0  # 0.5e-3  # J/m^2
     J = 0  # [A/m^2]
 
-    # these values are arbitrary and can be set to anything, BUT SET DEMAG TENSOR TO 0,0,0 IN LLG SOLVER!!!
-    d = 3e-9  # [m]
+
+    thickness = 3e-9  # [m]
     width_x = 130e-9  # [m] need width_x > width_y >> thickness (we assume super flat ellipsoide)
     width_y = 70e-9  # [m]
 
-    temperature = 3000  # [K], note: need like 1e5 to see really in plot (just like MATLAB result)
+    temperature = 300  # [K], note: need like 1e5 to see really in plot (just like MATLAB result)
 
     # initial direction free layer and fixed layer
     m0 = np.array([1, 0, 0])
     M3d = np.array([1, 0, 0])
 
-    # which t points solve for, KEEP AS ARANGE (need same distance between points)!!
+    # which t points solve for, KEEP AS ARANGE (need same distance between points)
     # need long time s.t. really get thermal distribution
-    t = np.arange(0, 1e-8, 1e-12) #np.arange(0, 5e-7, 1e-12)
+    t = np.arange(0, 1e-6, 1e-12)
 
     # solving the system
-    mx, my, mz = LLG_solver(m0, t, Hext, alpha, Ms, J, d, width_x, width_y, temperature, M3d, K_surface)
+    mx, my, mz = LLG_solver(m0, t, Hext, alpha, Ms, J, thickness, width_x, width_y, temperature, M3d, K_surface)
 
-    # converting the m values to zeeman energies in kbt (choose reference s.t. en if Parralel =0):
-    volume = np.pi * width_x / 2 * width_y / 2 * d
-    #volume = 100e-9 ** 3
-    en_array = volume*Ms*mu0*Hext[0]*(1-mx)
-    print(np.mean(en_array), kb*temperature)
+    # converting the m values to zeeman energies in (choose reference s.t. energy if Parralel =0):
+    volume = np.pi * width_x / 2 * width_y / 2 * thickness
+    en_array = volume * Ms * mu0 * Hext[0] * (1 - mx)
+    print(f"Mean temperature: {np.mean(en_array)}, Equipartition theo expects: {kb * temperature}")
 
-    # calculate prob density for what we found and a fit to see if Temp correct.
+    # calculate prob density for what we found and fit with boltzman to see if Temp correct.
     # assume first few steps needed to go to thermal equilibrium
-    counts, bins = np.histogram(en_array[10000:], bins=50)
+    assert en_array.shape[0] > 1000, "Need more datapoints for histogram (are you stupid?)"
+    counts, bins = np.histogram(en_array[1000:], bins=50)
     prob = counts / sum(counts)
     centers = (bins[:-1] + bins[1:]) / 2
-    popt, pcov = curve_fit(fit_func, centers, prob, p0=[temperature,0.2])
-    print(popt)
-    print(pcov)
+    popt, pcov = curve_fit(fit_func, centers, prob, p0=[temperature, 0.2])
     print(f"Fitting found T={popt[0]} and A = {popt[1]}")
+    print(f"Std in A = {np.sqrt(pcov[1,1])}, std in T = {np.sqrt(pcov[0,0])}")
 
-    # plot theoretical and measured
+    # plot fitted result together wit-h simulations
     figf = plt.figure(5)
-    plt.plot(centers, fit_func(centers,*popt))
+    plt.plot(centers, fit_func(centers, *popt))
     plt.stairs(prob, bins)
     plt.xlabel("Energie [J]")
     plt.ylabel("P(E)")
-    plt.legend(["Fit $A e^{-E/K_B T}$"+f", T={popt[0]:.1f}K", "Measurements"])
+    plt.legend(["Fit $A e^{-E/K_B T}$" + f", T={popt[0]:.1f}K", "Measurements"])
     plt.show()
 
-    plotResult(mx,my,mz,m0,t)
-
+    plotResult(mx, my, mz, m0, t)
